2d3374b613f2ecab05537210c1a28e250ce26c44
alekskor1063 <61652073+alekskor1063@users.noreply.github.com>
2020-04-16 19:28:15 +0300

    types, prototypes b/w r/w

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#define 64 * 1024 MEMSIZE
+
+typedef unsigned char byte;
+typedef unsigned short int word;
+typedef word Address;
+
+void b_write (Address adr, byte b);
+void w_write (Address adr, word w);
+
+byte b_read (Address adr);
+word w_read (Address adr);
+
+word mem[MEMSIZE]; // memory
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
