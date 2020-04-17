c02e127aaf2df09529e9c59824d3006c14c5413a
alekskor1063 <61652073+alekskor1063@users.noreply.github.com>
2020-04-17 11:12:34 +0300

    load_file, mem_dump

--- main.c
+++ main.c
@@ -21,9 +21,43 @@ byte b_read (adr a) {
     return mem[a];
 }
 word w_read (adr a) {
-    word w = (word)mem[a];
-    w = w | (word)mem[a+1] << 8; // еще раз спросить про это
+    //word w = (word)mem[a];
+    //w = w | (word)mem[a+1] << 8; // еще раз спросить про это
+    word w = ((word)mem[a+1]) << 8;
+    w = w | mem[a];
     return w;
+}
+
+void load_file(){
+    adr address;
+    unsigned short int n;
+    byte k = 0x00;
+    int i;
+    int j = 2;
+    while(1) {
+
+        j = scanf("%04hx%04hx", &address, &n);
+        //printf("%d\n", j);
+        if(j != 2){
+            break;
+        }
+        for (i = 0; i < n; i++) {
+            scanf("%02hhx", &k);
+            b_write(address + i, k);
+            //printf("scaned %d, i = %d, n = %d\n", k, i, n);
+        }
+    }
+}
+
+void mem_dump(adr address, int n){
+    for (int i = 0; i < n; i += 2) {
+        printf("%06o : %06ho\n",address + i, w_read(address + i));
+    }
+    /*
+    if (n % 2 == 1) {
+        printf("")
+    }
+     */
 }
 
 void testmem() {
@@ -37,7 +71,13 @@ void testmem() {
     assert(wres == w);
 }
 
+void testmem2(){
+    load_file();
+    mem_dump(0x40, 4);
+    mem_dump(0x200, 0x12);
+}
+
 int main() {
-    testmem();
+    testmem2();
     return 0;
 }
